6726702332939578cca84ccbef7096c3d0271ff4
zxjte9411 <a5852241@gmail.com>
2018-06-09 17:14:43 +0800

    init

---
+++ app/src/main/java/com/example/zxjte9411/androidfinalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zxjte9411.androidfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
