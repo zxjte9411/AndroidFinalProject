fb52e2e08c0ca8b3a12abff1e50f679b47e87ba4
zxjte9411 <a5852241@gmail.com>
2018-06-23 20:42:03 +0800

    新增 +向使用者獲取存取目錄權限 +讀取目錄(內部儲存空間) +使用ListView測試顯示

--- app/src/main/java/com/example/zxjte9411/androidfinalproject/PlayListFragment.java
+++ app/src/main/java/com/example/zxjte9411/androidfinalproject/PlayListFragment.java
@@ -1,12 +1,33 @@
 package com.example.zxjte9411.androidfinalproject;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 
 /**
@@ -22,13 +43,27 @@ public class PlayListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private Button btnTest;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    /*-------------------------------------*/
+    private File root, folder;
+    private String path;
     private OnFragmentInteractionListener mListener;
-
+    private ListView pathListView;
+    View.OnClickListener btnclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String[] list = folder.list();
+            ArrayList<String> tmp =new ArrayList<String>();
+            Log.v("wasa", String.valueOf(folder.getPath()));
+            Arrays.sort(list);
+            ArrayAdapter arrayAdapter = new ArrayAdapter(Objects.requireNonNull(getContext()),android.R.layout.simple_list_item_1,list);
+            pathListView.setAdapter(arrayAdapter);
+        }
+    };
     public PlayListFragment() {
         // Required empty public constructor
     }
@@ -58,10 +93,26 @@ public class PlayListFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        /*check permission for read external storage*/
+        checkReadFilePermissionAndEnbeIt(true);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btnTest = (Button) Objects.requireNonNull(getView()).findViewById(R.id.btn_test);
+        pathListView = Objects.requireNonNull(getView()).findViewById(R.id.path_list_view);
+//        pathListView.setOnItemClickListener();
+        btnTest.setOnClickListener(btnclick);
+        root = Environment.getExternalStorageDirectory();
+        path = Environment.getExternalStorageDirectory().getPath();
+        folder = new File(path);
+
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_play_list, container, false);
@@ -74,21 +125,70 @@ public class PlayListFragment extends Fragment {
         }
     }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                                               + " must implement OnFragmentInteractionListener");
-//        }
-//    }
+    /*
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                                               + " must implement OnFragmentInteractionListener");
+        }
+    }
+    */
+
+
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkReadFilePermissionAndEnbeIt(true);
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getContext(), "permission denied, boo!", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void checkReadFilePermissionAndEnbeIt(boolean on){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            } else {
+                // No explanation needed; request the permission
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        } else {
+            // Permission has already been granted
+            Toast.makeText(getContext(), "Permission has already been granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -105,4 +205,5 @@ public class PlayListFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
 }
