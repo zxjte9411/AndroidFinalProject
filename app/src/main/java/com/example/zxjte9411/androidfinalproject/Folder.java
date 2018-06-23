8ef0c86a1b461fb4c4ff35d2d364414fdf9585ff
zxjte9411 <a5852241@gmail.com>
2018-06-24 02:11:28 +0800

    獲取權限方式重寫 抓路徑幫放歌曲

--- app/src/main/java/com/example/zxjte9411/androidfinalproject/Folder.java
+++ app/src/main/java/com/example/zxjte9411/androidfinalproject/Folder.java
@@ -1,8 +1,11 @@
 package com.example.zxjte9411.androidfinalproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -29,8 +33,10 @@ public class Folder extends AppCompatActivity
     private final String previousPag = "..";
     private File root, folder;
     private String path;
-    private ArrayList<String> list;
+    private ArrayList<String> fileNameList;
+    private ArrayList<File> fileArrayList;
     private ListView pathListView;
+    MediaPlayer mediaPlayer;
     ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +59,17 @@ public class Folder extends AppCompatActivity
         pathListView.setOnItemClickListener(onItemClickListener);
         root = Environment.getExternalStorageDirectory();
         path = Environment.getExternalStorageDirectory().getPath();
-        folder = new File(path);
-        list = new ArrayList<String>();
-        list.add(previousPag);
-        list.addAll(Arrays.asList(folder.list(mediafilefilter)));
-        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        folder = new File(path + "/Music");
+        fileNameList = new ArrayList<String>();
+        fileNameList.add(previousPag);
+//        fileNameList.addAll(Arrays.asList(folder.list()));
+        for(int i = 0;i<folder.listFiles().length;i++){
+            fileNameList.add(folder.listFiles()[i].getName());
+        }
+        Collections.sort(fileNameList);
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fileNameList);
         pathListView.setAdapter(arrayAdapter);
+        mediaPlayer = new MediaPlayer();
     }
 
     @Override
@@ -95,7 +106,7 @@ public class Folder extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -132,25 +143,40 @@ public class Folder extends AppCompatActivity
     public AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            Toast.makeText(Folder.this, list.get(i), Toast.LENGTH_SHORT).show();
-            folder = new File(folder.getPath() + "/" + list.get(i));
-            Toast.makeText(Folder.this, folder.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-            list.clear();
-            list.add(previousPag);
-            list.addAll(Arrays.asList(folder.list(mediafilefilter)));
-            Collections.sort(list);
-            pathListView.setAdapter(new ArrayAdapter(Folder.this,android.R.layout.simple_list_item_1,list));
+            Toast.makeText(Folder.this, fileNameList.get(i), Toast.LENGTH_SHORT).show();
+            Uri uri = Uri.fromFile(new File(folder.getPath() + "/Anicca.mp3"));
+            if(mediaPlayer.isPlaying()){
+                mediaPlayer.release();
+                mediaPlayer.reset();
+            }
+            try {
+                mediaPlayer.setDataSource(Folder.this,uri);
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            folder = new File(folder.getPath() + "/" + list.get(i));
+//            Toast.makeText(Folder.this, folder.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+//            fileNameList.clear();
+//            fileNameList.add(previousPag);
+//            fileNameList.addAll(Arrays.asList(folder.list()));
+//            for(int k = 0;k<folder.listFiles().length;k++){
+//                fileNameList.add(folder.listFiles()[k].getName());
+//            }
+//            Collections.sort(fileNameList);
+//            pathListView.setAdapter(new ArrayAdapter(Folder.this,android.R.layout.simple_list_item_1,fileNameList));
         }
     };
 
-    FilenameFilter mediafilefilter = new FilenameFilter(){
-        private String[] filter = {".mp3",".ogg",".mp4"};
-        @Override
-        public boolean accept(File dir, String filename) {
-            for(int i= 0;i< filter.length ; i++){
-                if(filename.indexOf(filter[i]) != -1)return true;
-                else if(!dir.isFile())return true;
-            }
-            return false;
-        }};
+//    FilenameFilter mediafilefilter = new FilenameFilter(){
+//        private String[] filter = {".mp3",".ogg",".mp4"};
+//        @Override
+//        public boolean accept(File dir, String filename) {
+//            for(int i= 0;i< filter.length ; i++){
+//                if(filename.indexOf(filter[i]) != -1)return true;
+//                else if(!dir.isFile())return true;
+//            }
+//            return false;
+//        }};
 }
--- app/src/main/java/com/example/zxjte9411/androidfinalproject/Home.java
+++ app/src/main/java/com/example/zxjte9411/androidfinalproject/Home.java
@@ -2,12 +2,9 @@ package com.example.zxjte9411.androidfinalproject;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,16 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.Objects;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
 
+@RuntimePermissions
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HomePermissionsDispatcher.needsPermissionWithPermissionCheck(this);
+
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -38,7 +40,6 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
-        checkReadFilePermissionAndEnbeIt(true);
     }
 
     @Override
@@ -53,29 +54,20 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -110,48 +102,25 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         return true;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    checkReadFilePermissionAndEnbeIt(true);
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "permission denied, boo!", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void needsPermission() {
     }
 
-    private void checkReadFilePermissionAndEnbeIt(boolean on){
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            } else {
-                // No explanation needed; request the permission
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        } else {
-            // Permission has already been granted
-            Toast.makeText(this, "Permission has already been granted", Toast.LENGTH_SHORT).show();
-        }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        HomePermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void onShowRationale(final PermissionRequest request) {
+    }
+
+    @OnPermissionDenied({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void onPermissionDenied() {
+    }
+
+    @OnNeverAskAgain({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void onNeverAskAgain() {
     }
 }
