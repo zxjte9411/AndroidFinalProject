7d7b5c06dc3aaa7277c2ce210c1c5a25b2ebfbb0
zxjte9411 <a5852241@gmail.com>
2018-06-23 22:47:10 +0800

    可以進去資料夾裡面惹

--- app/src/main/java/com/example/zxjte9411/androidfinalproject/PlayListFragment.java
+++ app/src/main/java/com/example/zxjte9411/androidfinalproject/PlayListFragment.java
@@ -29,6 +29,7 @@ import java.io.FilenameFilter;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Objects;
 
 
@@ -45,34 +46,17 @@ public class PlayListFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            Toast.makeText(getContext(), list[i], Toast.LENGTH_SHORT).show();
-        }
-    };
-    private Button btnTest;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     /*-------------------------------------*/
+    private final String previousPag = "..";
     private File root, folder;
     private String path;
-    private String[] list;
+    private ArrayList<String> list;
     private OnFragmentInteractionListener mListener;
     private ListView pathListView;
-    View.OnClickListener btnclick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            list = folder.list();
-            ArrayList<String> tmp =new ArrayList<String>();
-            Log.v("wasa", String.valueOf(folder.getPath()));
-            Arrays.sort(list);
-            ArrayAdapter arrayAdapter = new ArrayAdapter(Objects.requireNonNull(getContext()),android.R.layout.simple_list_item_1,list);
-            pathListView.setAdapter(arrayAdapter);
-        }
-    };
-
+    ArrayAdapter arrayAdapter;
     public PlayListFragment() {
         // Required empty public constructor
     }
@@ -108,15 +92,16 @@ public class PlayListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        btnTest = (Button) Objects.requireNonNull(getView()).findViewById(R.id.btn_test);
         pathListView = Objects.requireNonNull(getView()).findViewById(R.id.path_list_view);
         pathListView.setOnItemClickListener(onItemClickListener);
-        btnTest.setOnClickListener(btnclick);
         root = Environment.getExternalStorageDirectory();
         path = Environment.getExternalStorageDirectory().getPath();
         folder = new File(path);
-
-
+        list = new ArrayList<String>();
+        list.add(previousPag);
+        list.addAll(Arrays.asList(folder.list(mediafilefilter)));
+        arrayAdapter = new ArrayAdapter(Objects.requireNonNull(getContext()),android.R.layout.simple_list_item_1,list);
+        pathListView.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -153,6 +138,19 @@ public class PlayListFragment extends Fragment {
     }
 
 
+    public AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Toast.makeText(getContext(), list.get(i), Toast.LENGTH_SHORT).show();
+            folder = new File(folder.getPath() + "/" + list.get(i));
+            Toast.makeText(getContext(), folder.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            list.clear();
+            list.add(previousPag);
+            list.addAll(Arrays.asList(folder.list(mediafilefilter)));
+            Collections.sort(list);
+            pathListView.setAdapter(new ArrayAdapter(Objects.requireNonNull(getContext()),android.R.layout.simple_list_item_1,list));
+        }
+    };
 
 
 
@@ -170,5 +168,17 @@ public class PlayListFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    FilenameFilter mediafilefilter = new FilenameFilter(){
+        private String[] filter = {".mp3",".ogg",".3gp",".mp4"};
+//        private String[] filter = {".mp3"};
+        @Override
+        public boolean accept(File dir, String filename) {
+            for(int i= 0;i< filter.length ; i++){
+                if(filename.indexOf(filter[i]) != -1)return true;
+                else if(!dir.isFile())return true;
+            }
+            return false;
+        }};
 
 }
